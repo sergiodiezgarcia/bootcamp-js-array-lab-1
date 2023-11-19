36a4dc93f398a08b75676367f9b38f6b4ba9488d
Sergio Diez <diezgarciasergioo@gmail.com>
2023-11-19 19:38:39 +0100

    punto 2

---
+++ src/2-a-protocolo-de-urgencia.ts
@@ -0,0 +1,18 @@
+import { Pacientes } from "./datos.js";
+
+export { pacientes } from "./datos.js";
+
+export const activarProtocoloUrgencia = (pacientes: Pacientes[]): boolean => {
+  let activarProctolo = false;
+  for (let i = 0; i < pacientes.length; i++) {
+    if (
+      pacientes[i].frecuenciaCardiaca > 100 &&
+      pacientes[i].temperatura > 39
+    ) {
+      activarProctolo = true;
+      break;
+    }
+  }
+
+  return activarProctolo;
+};
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { pacientes } from "./datos";
 import { obtenPacientesAsignadosAPediatria } from "./1-a-pacientes-pediatria";
 import {obtenPacientesAsignadosAPediatriaYMenorDeDiezAnios} from "./1-b-pacientes-pedíatria"
-
+import{activarProtocoloUrgencia} from "./2-a-protocolo-de-urgencia"
 const pacientesPediatria = obtenPacientesAsignadosAPediatria(pacientes);
 console.log("****** Pacientes de Pediatria");
 console.log(pacientesPediatria);
@@ -9,3 +9,9 @@ console.log(pacientesPediatria);
 const PacientesAsignadosAPediatriaYMenorDeDiezAnios = obtenPacientesAsignadosAPediatriaYMenorDeDiezAnios(pacientes);
 console.log("****** Pacientes de Pediatria menores de diez años");
 console.log(PacientesAsignadosAPediatriaYMenorDeDiezAnios);
+
+
+const activarProctolo = activarProtocoloUrgencia(pacientes);
+console.log("****** protocolo de urgencias");
+console.log(activarProctolo);
+
